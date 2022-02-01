4fd30e736dfae2a35602e42d715a8495bd0bf1a2
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-01-31 20:45:12 -0800

    made js functional for main recipe and add divs to html

--- script.js
+++ script.js
@@ -7,10 +7,36 @@ var mainVid = document.querySelector('#main-video');
 var mainInstructions = document.querySelector('#main-instr');
 var mainIngredients = document.querySelector('#main-ingre');
 
+var secondRecipe = document.querySelector('#main-recipe')
+var secondImg = document.querySelector('#second-img');
+var secondVid = document.querySelector('#second-video');
+var secondInstructions = document.querySelector('#second-instr');
+var secondIngredients = document.querySelector('#second-ingre');
+
+var thirdRecipe = document.querySelector('#main-recipe')
+var thirdImg = document.querySelector('#third-img');
+var thirdVid = document.querySelector('#third-video');
+var thirdInstructions = document.querySelector('#third-instr');
+var thirdIngredients = document.querySelector('#third-ingre');
+
+var fourthRecipe = document.querySelector('#main-recipe')
+var fourthImg = document.querySelector('#fourth-img');
+var fourthVid = document.querySelector('#fourth-video');
+var fourthInstructions = document.querySelector('#fourth-instr');
+var fourthIngredients = document.querySelector('#fourth-ingre');
+
+var fifthRecipe = document.querySelector('#main-recipe')
+var fifthImg = document.querySelector('#fifth-img');
+var fifthVid = document.querySelector('#fifth-video');
+var fifthInstructions = document.querySelector('#fifth-instr');
+var fifthIngredients = document.querySelector('#fifth-ingre');
+
 var recipeList = document.querySelector("#search-history")
+
 
 var recipeSearched = "";
 var mealId = "";
+var mealName = "";
 
 var savedRecipes = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
@@ -25,13 +51,39 @@ searchBtn.addEventListener('click', function(event){
 
     localStorage.setItem("searchHistory", JSON.stringify(savedRecipes));
  
-    getIngredientInfo()
+    getIngredientInfo();
+    createSearchHistory();
     
 });
 
 // need to create buttons for previously searched receipes, local storage made above
 function createSearchHistory() {
-    
+    recipeList.innerHTML = "";
+
+    var listOfSearchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+    if (listOfSearchHistory !== null) {
+        listOfRecipes = listOfSearchHistory
+    }
+
+    for (var i = 0; i < listOfRecipes.length; i++) {
+        var recipeButtons = document.createElement("button");
+
+        recipeButtons.textContent = listOfRecipes[i];
+
+        recipeList.appendChild(recipeButtons);
+
+        for (let i = 0; i <= recipeList.childElementCount; i++) {
+
+            if(recipeList.children.item(i) != null) {
+                recipeList.children.item(i).addEventListener("click", function () {
+                    console.log(recipeList.children.item(i));
+                    recipeSearched = recipeList.children.item(i).textContent
+                    getIngredientInfo();
+                });
+            }
+        }
+    }
 }
 
 function getIngredientInfo() {
@@ -49,15 +101,17 @@ function getIngredientInfo() {
                 mainRecipe.innerHTML = data.meals[0].strMeal;
                 mainImg.setAttribute('src', data.meals[0].strMealThumb);
             } else {
-                mainRecipe.innerHTML = "No Recipes Found"
+                mainRecipe.innerHTML = "No Recipes Found Please Try A Different Ingredient"
                 mainImg.setAttribute('src', "https://cdn.dribbble.com/users/1012566/screenshots/4187820/media/3cb974c28eb00627cc0671685c79ffd9.jpg");
                 mainVid.href = 'https://www.youtube.com/watch?v=INscMGmhmX4';
             }
             
             if(data.meals !== null){
                 mealName = data.meals[0].strMeal
-            } else {mealName = "No Recipes Found - Please Try A Different Ingredient"
+            } else {mealName = "No Recipes Found Please Try A Different Ingredient"
             }
+
+            console.log(mealName)
             
             // console.log(data.meals[0].strMeal)
 
@@ -74,11 +128,11 @@ function getIngredientInfo() {
                 if (data.meals !== null){
                     mainIngredients.innerHTML = "Ingredients: "
                     mainInstructions.innerHTML = "Instructions: " + data.meals[0].strInstructions 
-                    console.log(data.meals[0].strInstructions)
-                    console.log(Object.entries(data.meals[0]))
+                    // console.log(data.meals[0].strInstructions)
+                    // console.log(Object.entries(data.meals[0]))
                     dataArray = Object.entries(data.meals[0])
-                    console.log(dataArray.slice(9,49))
-                    console.log(dataArray[10][0], dataArray[10][1])
+                    // console.log(dataArray.slice(9,49))
+                    // console.log(dataArray[10][0], dataArray[10][1])
                     for (each of dataArray.slice(9,29)) {
                         if(each[1] != "") {
                           console.log(each[1], dataArray[dataArray.indexOf(each)+20][1])
@@ -88,6 +142,7 @@ function getIngredientInfo() {
      
                     for (each of dataArray.slice(9,29)) {
                         if(each[1] != "") {
+                            console.log(each)
                             const newIngredient = document.createElement('div')
                             newIngredient.textContent = each[1] +' :   ' + dataArray[dataArray.indexOf(each)+20][1]
                             mainIngredients.append(newIngredient)
@@ -107,4 +162,4 @@ function getIngredientInfo() {
 }
 
 createSearchHistory();
-// getIngredientInfo()
+getIngredientInfo()
